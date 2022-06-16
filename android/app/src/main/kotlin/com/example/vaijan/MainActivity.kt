1c4210d9b428ea1b1f85669c3b99bc2e1e2bf666
Mou Biswas <qtmeaowmou16@gmail.com>
2022-06-16 10:18:59 +0600

    day 1

---
+++ android/app/src/main/kotlin/com/example/vaijan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vaijan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:vaijan/screens/home_page.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Vaijan",
+      home: HomePage(),
+    ),
+  );
+}
