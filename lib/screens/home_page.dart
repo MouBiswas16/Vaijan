ddd03242e8986700762744b4a6d78ef5999bb3ad
Mou Biswas <qtmeaowmou16@gmail.com>
2022-06-18 00:12:09 +0600

    day 2

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -14,6 +14,7 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.only(left: 10, right: 10),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Image.asset(
               "assets/images/Vaijan Logo.png",
@@ -37,25 +38,91 @@ class HomePage extends StatelessWidget {
               ),
             ),
             SizedBox(height: 13),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                "Connect with Phone Number",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+            SizedBox(
+              height: 100,
+              width: 300,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Connect with Phone Number",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Color.fromARGB(0, 42, 237, 139)),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(208, 42, 237, 139)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
                   ),
                 ),
               ),
+            ),
+            SizedBox(
+              height: 100,
+              width: 300,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Register",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(208, 42, 237, 139)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 12),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Icon(Icons.ac_unit),
+                      Text("Google"),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 8),
+                TextButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Icon(Icons.abc_sharp),
+                      Text("Facebook"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 12),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Text("English"),
+                      Icon(Icons.pin_drop),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
