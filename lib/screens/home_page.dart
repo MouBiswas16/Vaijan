4922a88bfcdcdac7ea68b013145648a56baa4611
Mou Biswas <qtmeaowmou16@gmail.com>
2022-06-20 21:06:19 +0600

    day 3

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.white,
       extendBodyBehindAppBar: true,
       body: Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10),
+        padding: const EdgeInsets.only(left: 50, right: 50),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -20,105 +20,152 @@ class HomePage extends StatelessWidget {
               "assets/images/Vaijan Logo.png",
               scale: 3,
             ),
+            SizedBox(height: 15),
             Text(
               "Welcome Back",
               style: TextStyle(
-                color: Colors.black,
-                fontSize: 48,
+                color: Color.fromARGB(255, 32, 32, 32),
+                fontSize: 30,
                 fontWeight: FontWeight.bold,
+                letterSpacing: 1.5,
               ),
             ),
-            SizedBox(height: 5),
+            SizedBox(height: 10),
             Text(
               "Login to continue",
               style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.normal,
+                color: Color.fromARGB(255, 41, 41, 41),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.5,
               ),
             ),
-            SizedBox(height: 13),
+            SizedBox(height: 30),
             SizedBox(
-              height: 100,
-              width: 300,
-              child: TextButton(
+              child: ElevatedButton(
                 onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  primary: Color.fromARGB(255, 164, 208, 95),
+                  minimumSize: Size.fromHeight(50),
+                ),
                 child: Text(
                   "Connect with Phone Number",
                   style: TextStyle(
-                    color: Colors.black,
+                    color: Color.fromARGB(255, 74, 74, 74),
                     fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      Color.fromARGB(208, 42, 237, 139)),
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18),
-                    ),
+                    fontSize: 14,
+                    letterSpacing: 1.5,
                   ),
                 ),
               ),
             ),
+            SizedBox(height: 15),
             SizedBox(
-              height: 100,
-              width: 300,
-              child: TextButton(
+              child: ElevatedButton(
                 onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  primary: Color.fromARGB(255, 234, 234, 234),
+                  minimumSize: Size.fromHeight(50),
+                ),
                 child: Text(
                   "Register",
                   style: TextStyle(
-                    color: Colors.black,
+                    color: Color.fromARGB(255, 74, 74, 74),
                     fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      Color.fromARGB(208, 42, 237, 139)),
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18),
-                    ),
+                    fontSize: 14,
+                    letterSpacing: 1.5,
                   ),
                 ),
               ),
             ),
             SizedBox(height: 12),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                TextButton(
-                  onPressed: () {},
-                  child: Row(
-                    children: [
-                      Icon(Icons.ac_unit),
-                      Text("Google"),
-                    ],
+                SizedBox(
+                  height: 40,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      primary: Colors.white,
+                      // minimumSize: Size.fromHeight(50),
+                      minimumSize: Size.fromWidth(200),
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "assets/images/Google Logo.png",
+                          scale: 35,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "Google",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 81, 81, 81),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                SizedBox(width: 8),
-                TextButton(
-                  onPressed: () {},
-                  child: Row(
-                    children: [
-                      Icon(Icons.abc_sharp),
-                      Text("Facebook"),
-                    ],
+                // SizedBox(width: 8),
+                SizedBox(
+                  height: 40,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      primary: Colors.white,
+                      minimumSize: Size.fromWidth(200),
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "assets/images/Facebook Logo.webp",
+                          scale: 18,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "Facebook",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 81, 81, 81),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                )
+                ),
               ],
             ),
-            SizedBox(height: 12),
+            SizedBox(height: 30),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 TextButton(
                   onPressed: () {},
                   child: Row(
                     children: [
-                      Text("English"),
-                      Icon(Icons.pin_drop),
+                      Text(
+                        "English",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 92, 92, 92)),
+                      ),
+                      SizedBox(width: 5),
+                      Icon(
+                        Icons.arrow_drop_down,
+                        color: Color.fromARGB(255, 81, 81, 81),
+                      ),
                     ],
                   ),
                 ),
