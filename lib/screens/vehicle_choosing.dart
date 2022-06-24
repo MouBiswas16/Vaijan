a0960c2fa40495fb1f915c936533ec12658d7485
Mou Biswas <qtmeaowmou16@gmail.com>
2022-06-25 00:35:50 +0600

    day 7

--- lib/screens/vehicle_choosing.dart
+++ lib/screens/vehicle_choosing.dart
@@ -1,57 +1,52 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vaijan/screens/custom_appbar.dart';
+import 'package:vaijan/screens/custom_bottom_nav_bar.dart';
 
-class VehicleChoosing extends StatelessWidget {
+class VehicleChoosing extends StatefulWidget {
   const VehicleChoosing({Key? key}) : super(key: key);
 
+  @override
+  State<VehicleChoosing> createState() => _VehicleChoosingState();
+}
+
+class _VehicleChoosingState extends State<VehicleChoosing> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Color(0xffF4F4F4),
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(70),
           child: CustomAppBar(),
         ),
-        body: Container(),
-        bottomNavigationBar: SizedBox(
-          height: 70,
-          child: BottomNavigationBar(
-            backgroundColor: Color(0xff612A6F),
-            unselectedItemColor: Colors.white,
-            unselectedLabelStyle: TextStyle(
-              // color: Colors.white,
-              fontSize: 12,
-            ),
-            type: BottomNavigationBarType.fixed,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(
-                  CupertinoIcons.home,
-                  color: Colors.white,
-                ),
-                label: "হোম",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.qr_code,
-                  color: Colors.white,
-                ),
-                label: "QR স্ক্যান",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  CupertinoIcons.envelope,
-                  color: Colors.white,
-                ),
-                label: "ইনবক্স",
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: RadialGradient(
+                        colors: [
+                          Color.fromARGB(255, 129, 129, 129),
+                          Colors.white,
+                        ],
+                      ),
+                    ),
+                    child: Center(
+                      child: CircleAvatar(),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
         ),
+        bottomNavigationBar: CustomBottomNavBar(),
       ),
     );
   }
